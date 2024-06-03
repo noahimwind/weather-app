1f633950052b53683f045affc5fa4059a098efc9
Noah <ninadurkes@gmail.com>
2024-06-03 19:50:31 +0200

    users can select a city and the content changes accordingly

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This is a weather web-app.
+
+Built with:
+- HTML, CSS, JavaScript
+- API by OpenWeatherApp
+- [youtube tutorial](https://www.youtube.com/watch?v=MIYQR-Ybrn4&list=PLjwm_8O3suyOgDS_Z8AWbbq3zpCmR-WE9&ab_channel=GreatStack)
+
+Learnings:
+- how to get an API Key and integrate it into your project
+- how to read API URLs and change them dynamically
+- how to integrate the fetched data into my web-app
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const apiKey = "1d4d8d9a7c39a3bc6c57173cd49492c4";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    var data = await response.json();
-    document.querySelector(".city"),innerHTML = data.name;
-    document.querySelector(".temp"),innerHTML = data.main.temp;
-    document.querySelector(".humidity"),innerHTML = data.main.humidity;
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`); 
+    var data = await response.json();   // parses json response into a javascript object
+
+    // updates the HTML content
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
